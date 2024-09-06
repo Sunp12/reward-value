8f6ac46550ee010b13cdaf57fffa69effe59f42a
Sunil Patidar <sp415653@gmail.com>
2024-09-06 13:52:11 +0530

    RewardValue class Created.

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,20 @@
+public class RewardValue {
+    private double cashValue;
+    private int miles;
+
+    public RewardValue(double cashValue) {
+        this.cashValue=cashValue;
+    }
+    public RewardValue(int miles) {
+        this.miles=miles;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+
+    public int getMilesValue() {
+        return this.miles;
+    }
+}
